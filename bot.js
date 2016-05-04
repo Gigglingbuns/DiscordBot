e06078f1b0d046f474c23846dcba5a54127516d4
Mark Danison <darktacos123@gmail.com>
2016-05-03 21:22:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,31 @@ var party = "";
 var pingPongRunning = false,
     pingPongClient;
 
-//var people = {"hardeep","1it","crypton","nucleus","sheep","touka","jaws","reaper","goku","anna","peripsis","inferno","odyssey","cursed","acryuz","rampage","rubik","legend","muffin"};
+//var peopleToAdd = {"inferno","odyssey","cursed","acryuz","rampage","rubik","legend","muffin"};
 
-function people(){
-    this.coins = 0;
+var person = function () { this.coins = 0;  } 
+person.prototype.addCoins = function(amount){ 
+    this.coins += amount;  
 }
-//people.prototype.coins = 0;
-var hardeep = new people();
-function addCoins(ammount){
-    this.coins += ammount;
-}
+var hardeep = new person();
+var 1it = new person();
+var crypton = new person();
+var nucleus = new person();
+var sheep = new pesron();
+var touka = new person();
+var jaws = new person();
+var reaper = new person();
+var goku = new person();
+var anna = new person();
+var sway = new person();
+var peripsis = new person();
+var odyssey = new person();
+var cursed = new person();
+var acryuz = new person();
+var rampage = new person();
+var rubik = new person();
+var legend = new person();
+
 function getEmail() {
     if(document.getElementById("email").value)
         return document.getElementById("email").value;
@@ -31,15 +46,15 @@ function toggleStateOfPingPong() {
     if(pingPongRunning) {
         pingPongClient.logout();
         pingPongClient = null;
-        document.getElementById("pingpongoutput").innerHTML = "Client terminated...";
+        document.getElementById("pingpongoutput").innerHTML = "Client farted and died";
         pingPongRunning = false;
-        document.getElementById("pingpongbutton").innerHTML = "RUN";
+        document.getElementById("pingpongbutton").innerHTML = "ACTIVATE YOUR GAY BOT";
         document.getElementById("pingpongbutton").className = "startButton";
     } else {
         document.getElementById("pingpongoutput").innerHTML = "";
         startPingPong();
         pingPongRunning = true;
-        document.getElementById("pingpongbutton").innerHTML = "STOP";
+        document.getElementById("pingpongbutton").innerHTML = "IMMEDIATE SHUTDOWN";
         document.getElementById("pingpongbutton").className = "stopButton";
     }
 }
@@ -51,7 +66,7 @@ function startPingPong() {
     }
 
     function error(e) {
-        output(e + " - <i><u>are you sure your credentials are correct?</u></i>");
+        output(e + " - <i><u>are you sure your user and pass is correct?</u></i>");
     }
 
     pingPongClient = new Discord.Client();
@@ -65,17 +80,33 @@ function startPingPong() {
 
     pingPongClient.on("message", function(msg) {
 
-        if(msg.content === "!party") {
+        if(msg.content.substring(0,7) == "addcoin"){
+            if(!isNaN(parseFloat(n)) && isFinite(n)){
+               if(msg.content.substring()==){
+            /*    
+                } else if(){
+                
+                } else if(){
+                
+                } else {
+                 this.reply(msg,"no user found");   
+                }
+            */ } else {
+                 this.reply(msg,"no ammount to be added found");
+             }
+        }
+        
+        if(msg.content === "party") {
             this.reply(msg, party);
             output("pong'd <b>" + msg.sender.username + "</b>");
         }
-        if(msg.content.substring(0, 9) == "!setparty") {
+        if(msg.content.substring(0, 9) == "setparty") {
             party = msg.content.substring(10, msg.content.length);
             this.reply(msg, "party set to " + party);
             output("pong'd <b>" + msg.sender.username + "</b>");
         }
         if(msg.content=="hardeep"){
-            this.reply(msg,"I've never watched porn");
+            this.reply(msg,"'I've never watched porn' - Hardeep 2016");
         }
         if(msg.content === "cursed") {
             this.reply(msg, "u have been cursed 4 lyfe now go die in a hole and jump into traffic no one cares and KYS");
