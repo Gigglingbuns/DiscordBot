520e5e006034cf3cee6871308998a82264d3102e
Not ʍi૮ђα૯ℓ <darktacos123@gmail.com>
2016-06-16 15:54:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,7 +114,7 @@ function startPingPong() {
     if(msg.content=="cryp"){
       this.reply(msg,"I'm gay                                                                     (suggested by goku)");
     }
-    var dialogues = ["yes","no","No, not in a million years","My subconscious tells me no","Nope, definitely not","I don't think so, but I could be wrong","Nope, not a chance","Probably, I don't see why not","My spiritual ancestors confirms, it is surely to happen!","Yes, it will happen 200%","My level of clairvoyance isn't proficient enough, ask botchan","Yes, I am completely certain"];
+    var dialogues = ["f*** off sir","yes","no","No, not in a million years","My subconscious tells me no","Nope, definitely not","I don't think so, but I could be wrong","Nope, not a chance","Probably, I don't see why not","My spiritual ancestors confirms, it is surely to happen!","Yes, it will happen 200%","My level of clairvoyance isn't proficient enough, ask botchan","Yes, I am completely certain"];
     if(msg.content.substring(0,6).toLowerCase()=="should"||msg.content.substring(0,5).toLowerCase()=="would"){
       var picker = Math.floor(Math.random()*(dialogues.length-1));
       this.reply(msg,dialogues[picker])
